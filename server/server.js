ea7e0f4efb1119d679cf04fad3304fd395736847
rsensenig <r.a.sensenig@gmail.com>
2022-10-29 18:11:20 -0400

    .gitignore is now working

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const router = express.Router();
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
+require('./db/connection');
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
   });
